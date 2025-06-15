8865e90a3fbec0a48fda5920d593c0aa912ed905
MANISH CHAUDHARY <132375487+Chaudhary626@users.noreply.github.com>
2025-06-15 19:26:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-TELEGRAM_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"  # <-- Set your token here
+TELEGRAM_TOKEN = "6396720642:AAGa3uj9UAPbUeWP6DwcB4-iImDP4C6RHyI"  # <-- Set your token here
 
 DB_URI = "sqlite+aiosqlite:///bot/database/db.sqlite3"  # SQLite for dev, change to Postgres in prod
 
-ADMIN_IDS = [123456789]  # Telegram user IDs of admins
+ADMIN_IDS = [5718213826]  # Telegram user IDs of admins
 TASK_TIMEOUT_MINUTES = 20
 MAX_VIDEO_COUNT = 5
 MAX_VIDEO_DURATION = 5 * 60  # seconds
